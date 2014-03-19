cf4ad7c6c3d3ab6344d2b74429ef026424c4b8af
User <User@anufreychukvs>
2014-03-19 15:16:35 +0600

    В пакете modelStructure добавлено enum FieldTypes

---
+++ src/com/prim/core/modelStructure/FieldTypes.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.prim.core.modelStructure;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * типы данных в полях модели
+ * 
+ * @author Rice Pavel
+ */
+public enum FieldTypes {
+  
+  INT, BOOL, CHAR, DATETIME, DECIMAL, TEXT, REF; 
+  
+}
